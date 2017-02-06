e44fd9e0301bfed1bbb13f97cb9251234a2c5951
bujordet <mo.bujordet@gmail.com>
2017-02-06 15:17:30 +0100

    Initialized django project

---
+++ agnitio/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mobujordet/Documents/skole/PU/TDT4140-Group75/agnitio/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
